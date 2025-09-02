1ad3059c0bac62f3d04bf812ffad2a08ca4aae4c
viktorbeck98 <viktor.beck98@gmail.com>
2025-09-02 13:18:09 +0200

    add base structure + add initial implementations for components

---
+++ components/Base/ParserBase.py
@@ -0,0 +1,33 @@
+from abc import ABC, abstractmethod
+from typing import Any, Literal, Optional
+from components.Base.ConfigBase import ConfigBase
+from components.utils.DataBuffer import DataBuffer
+from schemas.BaseSchema import BaseSchema
+
+class ParserBase(ABC):
+    def __init__(self, buffer_mode: Optional[Literal["no_buf", "batch", "window"]] = "no_buf", buffer_size: Optional[int] = None, config: Optional[ConfigBase | dict] = None):
+        self.data_buffer = DataBuffer(
+            mode=buffer_mode,
+            size=buffer_size,
+            process_function=self.parse,
+        )
+        if isinstance(config, dict):
+            self.config = ConfigBase(**config)
+        elif isinstance(config, ConfigBase):
+            self.config = config
+        else:
+            self.config = ConfigBase()
+
+    @abstractmethod
+    def parse(self, data: Any, config: ConfigBase):
+        pass
+
+    @abstractmethod
+    def train(self, data: Any, config: ConfigBase) -> None:
+        pass
+
+    def _process(self, data: BaseSchema, config: ConfigBase) -> BaseSchema:
+        # schema validation?
+        if config:
+            self.config.update_config(config.get_config())
+        return self.data_buffer.add(data)
