775aa8c942c0dc1b5ded42774337e71209831178
viktorbeck98 <viktor.beck98@gmail.com>
2025-10-03 11:13:02 +0200

    update schema class

--- src/schemas/_op.py
+++ src/schemas/_op.py
@@ -1,11 +1,13 @@
 """" Interface between the code base and the protobuf code."""
 import src.schemas.schemas_pb2 as s
 
-from typing import NewType, Tuple, Dict
+from typing import NewType, Tuple, Dict, Type, Union
+from google.protobuf.message import Message
 
 
 #  Main variables ************************************
-SchemaT = NewType("SchemaT", object)
+# Use Union of actual protobuf classes for better type hints
+SchemaT = Union[s.Schema, s.LogSchema, s.ParserSchema, s.DetectorSchema]
 SchemaID = NewType("SchemaID", bytes)
 
 
@@ -16,11 +18,11 @@ DETECTOR_SCHEMA: SchemaID = SchemaID(b"3")
 
 
 __current_version = "1.0.0"
-__id_codes: Dict[SchemaID, SchemaT] = {
-    BASE_SCHEMA: s.Schema,  # type: ignore
-    LOG_SCHEMA: s.LogSchema,  # type: ignore
-    PARSER_SCHEMA: s.ParserSchema,  # type: ignore
-    DETECTOR_SCHEMA: s.DetectorSchema,  # type: ignore
+__id_codes: Dict[SchemaID, Type[Message]] = {
+    BASE_SCHEMA: s.Schema,
+    LOG_SCHEMA: s.LogSchema,
+    PARSER_SCHEMA: s.ParserSchema,
+    DETECTOR_SCHEMA: s.DetectorSchema,
 }
 
 
@@ -36,11 +38,11 @@ class IncorrectSchema(Exception):
 
 
 # Private methods *************************************
-def __get_schema(schema_id: SchemaID) -> SchemaT | NotSupportedSchema:
+def __get_schema_class(schema_id: SchemaID) -> Type[Message]:
+    """Get the schema class for the given schema ID."""
     if schema_id not in __id_codes:
         raise NotSupportedSchema()
-
-    return SchemaT(__id_codes[schema_id])
+    return __id_codes[schema_id]
 
 
 # Main methods *****************************************
@@ -48,7 +50,8 @@ def initialize(schema_id: SchemaID, **kwargs) -> SchemaT:
     """Initialize a protobuf schema, it use its arguments and the assigned
     id."""
     kwargs["__version__"] = __current_version
-    return __get_schema(schema_id)(**kwargs)  # type: ignore
+    schema_class = __get_schema_class(schema_id)
+    return schema_class(**kwargs)
 
 
 def serialize(id_schema: SchemaID, schema: SchemaT) -> bytes:
@@ -56,12 +59,14 @@ def serialize(id_schema: SchemaID, schema: SchemaT) -> bytes:
 
     First 4 bits are the schema id
     """
-    return id_schema + schema.SerializeToString()  # type: ignore
+    return id_schema + schema.SerializeToString()
 
 
 def deserialize(message: bytes) -> Tuple[SchemaID, SchemaT]:
     """Return the schema and id from a serialize message."""
-    schema = __get_schema(schema_id := SchemaID(message[:1]))()  # type: ignore
+    schema_id = SchemaID(message[:1])
+    schema_class = __get_schema_class(schema_id)
+    schema = schema_class()
     schema.ParseFromString(message[1:])
     return schema_id, schema
 
