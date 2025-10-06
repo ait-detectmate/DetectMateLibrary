286fa1043dd260b2b7410f6c8b533bdafffa2428
angre.garcia-gomez@ait.ac.at <angre.garcia-gomez@ait.ac.at>
2025-10-06 16:10:57 +0200

    ADD buffer test

--- tests/test_CoreDetector.py
+++ tests/test_CoreDetector.py
@@ -43,6 +43,20 @@ class MockupDetector_window(CoreDetector):
         pass
 
 
+class MockupDetector_buffer(CoreDetector):
+    def __init__(self, name: str, config: DetectorConfig) -> None:
+        super().__init__(
+            name=name, buffer_mode="window", buffer_size=3, config=config
+        )
+
+    def detect(self, input_, output_):
+        output_.score = 0.9
+        output_.predictionLabel = True
+
+    def train(self, data):
+        pass
+
+
 class TestCoreDetector:
     def test_initialize_default(self) -> None:
         detector = MockupDetector(name="TestDetector", config={})
@@ -92,6 +106,28 @@ class TestCoreDetector:
         assert result == expected_result, f"result -> {result}"
 
     def test_process_input_schema_not_serialized_window_3(self) -> None:
+        detector = MockupDetector_window(name="TestDetector", config=MockupConfig())
+        expected_result = schemas.initialize(schemas.DETECTOR_SCHEMA, **{
+            "__version__": "1.0.0",
+            "detectorID": "TestDetector01",
+            "detectorType": "TestType",
+            "alertID": 0,
+            "detectionTimestamp": 0,
+            "predictionLabel": True,
+            "score": 0.9,
+            "extractedTimestamps": [12121, 12121, 12121]
+        })
+        data = schemas.initialize(schemas.PARSER_SCHEMA, **{
+            "logFormatVariables": {"timestamp": "12121"}, "log": "This is a parsed log."
+        })
+
+        assert detector.process(data) is None
+        assert detector.process(data) is None
+
+        result = detector.process(data)
+        assert result == expected_result, f"result -> {expected_result} and {result}"
+
+    def test_process_input_schema_not_serialized_buffer_3(self) -> None:
         detector = MockupDetector_window(name="TestDetector", config=MockupConfig())
         expected_result = schemas.initialize(schemas.DETECTOR_SCHEMA, **{
             "__version__": "1.0.0",
