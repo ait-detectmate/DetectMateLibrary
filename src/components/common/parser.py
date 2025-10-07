7f88bd0455d8b574f0424b3689f7b7846afefbef
angre.garcia-gomez@ait.ac.at <angre.garcia-gomez@ait.ac.at>
2025-10-07 11:59:43 +0200

    ADD first version of CoreParser

--- src/components/common/parser.py
+++ src/components/common/parser.py
@@ -1,36 +1,70 @@
 from src.components.common.core import CoreComponent, ConfigCore
 from src.components.utils.data_buffer import ArgsBuffer
+import src.components.utils.id_generator as op
+
 import src.schemas as schemas
 
-from typing import Any, Literal, Optional
+from abc import ABC, abstractmethod
+from typing import Any, Optional
 
 
-class CoreParser(CoreComponent):
+class CoreParserConfig(ConfigCore):
+    parserType: str = "<PLACEHOLDER>"
+
+    start_id: int = 0
+
+
+def _generate_default_output(
+    input_: schemas.LogSchema, config: CoreParserConfig
+) -> schemas.ParserSchema:
+    return schemas.initialize(
+        schema_id=schemas.PARSER_SCHEMA,
+        **{
+            "__version__": "1.0.0",
+            "parserType": config.parserType,
+            "EventID": 0,
+            "template": "",
+            "parserID": 0,
+            "logID": input_.logID,
+            "log": input_.log,
+        }
+    )
+
+
+class CoreParser(CoreComponent, ABC):
     def __init__(
         self,
         name: str = "CoreParser",
-        buffer_mode: Optional[Literal["no_buf", "batch", "window"]] = "no_buf",
-        buffer_size: Optional[int] = None,
-        config: Optional[ConfigCore | dict] = ConfigCore()
+        config: Optional[CoreParserConfig | dict] = CoreParserConfig(),
+        id_generator: op.SimpleIDGenerator = op.SimpleIDGenerator,
     ):
         if isinstance(config, dict):
-            config = ConfigCore.from_dict(config)
+            config = CoreParserConfig.from_dict(config)
 
         super().__init__(
             name=name,
             type_="Parser",
             config=config,
             train_function=self.train,
-            process_function=self.parse,
-            args_buffer=ArgsBuffer(
-                mode=buffer_mode, size=buffer_size
-            ),
+            process_function=self.run,
+            args_buffer=ArgsBuffer(mode="no_buf", size=None),
             input_schema=schemas.LOG_SCHEMA,
             output_schema=schemas.PARSER_SCHEMA,
         )
+        self.id_generator = id_generator(self.config.start_id)
 
-    def parse(self, data: Any, config: ConfigCore):
-        pass
+    def run(self, input_: schemas.LogSchema) -> schemas.ParserSchema:
+        output_ = _generate_default_output(input_=input_, config=self.config)
+
+        self.parse(input_=input_, output_=output_)
+        output_.parserID = self.id_generator()
+        return output_
+
+    @abstractmethod
+    def parse(
+        self, input_: schemas.LogSchema, output_: schemas.ParserSchema
+    ) -> None:
+        return
 
     def train(self, data: Any, config: ConfigCore) -> None:
         pass
---
+++ tests/test_CoreParser.py
@@ -0,0 +1,75 @@
+from src.components.common.parser import CoreParser, CoreParserConfig
+import src.schemas as schemas
+
+import pydantic
+import pytest
+
+
+class MockupConfig(CoreParserConfig):
+    parserType: str = "TestType"
+
+
+class MockupParser(CoreParser):
+    def __init__(self, name: str, config: CoreParserConfig) -> None:
+        super().__init__(name=name, config=config)
+
+    def parse(self, input_, output_):
+        output_.EventID = 1
+        output_.template = "hello"
+        output_.variables.extend(["a", "b"])
+        output_.logFormatVariables["t"] = "c"
+
+    def train(self, data):
+        pass
+
+
+class TestCoreParser:
+    def test_initialize_default(self) -> None:
+        parser = MockupParser(name="TestParser", config={})
+
+        assert isinstance(parser, CoreParser)
+        assert parser.name == "TestParser"
+        assert isinstance(parser.config, CoreParserConfig)
+        assert parser.input_schema == schemas.LOG_SCHEMA
+        assert parser.output_schema == schemas.PARSER_SCHEMA
+
+    def test_incorrect_config_type(self) -> None:
+        with pytest.raises(pydantic.ValidationError):
+            MockupParser(name="TestParser", config={"param1": "invalid_type", "param2": 0.5})
+
+    def test_process_correct_input_schema(self) -> None:
+        parser = MockupParser(name="TestParser", config={})
+        data = schemas.initialize(schemas.LOG_SCHEMA, **{
+            "logID": 1, "log": "This is a log."
+        })
+        data_serialized = schemas.serialize(schemas.LOG_SCHEMA, data)
+        result = parser.process(data_serialized)  # no error should be produced
+        assert isinstance(result, bytes)  # and result should be bytes
+
+    def test_process_incorrect_input_schema(self) -> None:
+        parser = MockupParser(name="TestParser", config={})
+        data = schemas.initialize(schemas.DETECTOR_SCHEMA, **{"score": 0.99})
+        data_serialized = schemas.serialize(schemas.DETECTOR_SCHEMA, data)
+        with pytest.raises(schemas.IncorrectSchema):
+            parser.process(data_serialized)
+
+    def test_process_correct_input_schema_not_serialize(self) -> None:
+        parser = MockupParser(name="TestParser", config=MockupConfig())
+        expected_result = schemas.initialize(schemas.PARSER_SCHEMA, **{
+            "__version__": "1.0.0",
+            "parserType": "TestType",
+            "EventID": 1,
+            "template": "hello",
+            "parserID": 0,
+            "logID": 1,
+            "log": "This is a log."
+        })
+        expected_result.variables.extend(["a", "b"])
+        expected_result.logFormatVariables["t"] = "c"
+
+        data = schemas.initialize(schemas.LOG_SCHEMA, **{
+            "logID": 1, "log": "This is a log."
+        })
+        result = parser.process(data)
+
+        assert result == expected_result, f"results {result} and expected {expected_result}"
