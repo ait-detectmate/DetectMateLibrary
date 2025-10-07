25e2a9c33ece89f9c010491a71115804f988ab66
angre.garcia-gomez@ait.ac.at <angre.garcia-gomez@ait.ac.at>
2025-10-07 15:34:35 +0200

    Add pipeline tests

---
+++ tests/test_pipelines.py
@@ -0,0 +1,92 @@
+
+from src.components.common.detector import CoreDetector, CoreDetectorConfig
+from src.components.common.parser import CoreParser, CoreParserConfig
+
+from src.components.readers.log_file import LogFileConfig, LogFileReader
+
+
+class MockupParser(CoreParser):
+    def parse(self, input_, output_):
+        output_.EventID = 1
+        output_.template = "hello"
+        output_.variables.extend(["a", "b"])
+        output_.logFormatVariables["timestamp"] = "1"
+
+
+class MockupDetector(CoreDetector):
+    def detect(self, input_, output_):
+        output_.score = 0.9
+        output_.predictionLabel = True
+
+
+class TestCaseBasicPipelines:
+    """This pipelines should not crash."""
+    def test_basic_pipeline(self) -> None:
+        reader = LogFileReader(
+            config=LogFileConfig(file="tests/test_folder/logs.log")
+        )
+        parser = MockupParser(config=CoreParserConfig(
+            parserType="TestParserType"
+        ))
+        detector = MockupDetector(
+            config=CoreDetectorConfig(
+                detectorID="TestID",
+                detectorType="TestDetectorType",
+            ),
+            buffer_mode="no_buf",
+            buffer_size=None,
+        )
+
+        log = reader.process(as_bytes=False)
+        parsed_log = parser.process(log)
+        assert detector.process(parsed_log) is not None
+
+    def test_window_pipeline(self) -> None:
+        reader = LogFileReader(
+            config=LogFileConfig(file="tests/test_folder/logs.log")
+        )
+        parser = MockupParser(config=CoreParserConfig(
+            parserType="TestParserType"
+        ))
+        detector = MockupDetector(
+            config=CoreDetectorConfig(
+                detectorID="TestID",
+                detectorType="TestDetectorType",
+            ),
+            buffer_mode="window",
+            buffer_size=3,
+        )
+
+        for _ in range(2):
+            log = reader.process(as_bytes=False)
+            parsed_log = parser.process(log)
+            assert detector.process(parsed_log) is None
+
+        log = reader.process(as_bytes=False)
+        parsed_log = parser.process(log)
+        assert detector.process(parsed_log) is not None
+
+    def test_batch_pipeline(self) -> None:
+        reader = LogFileReader(
+            config=LogFileConfig(file="tests/test_folder/logs.log")
+        )
+        parser = MockupParser(config=CoreParserConfig(
+            parserType="TestParserType"
+        ))
+        detector = MockupDetector(
+            config=CoreDetectorConfig(
+                detectorID="TestID",
+                detectorType="TestDetectorType",
+            ),
+            buffer_mode="batch",
+            buffer_size=3,
+        )
+
+        for _ in range(2):
+            log = reader.process(as_bytes=False)
+            parsed_log = parser.process(log)
+            assert detector.process(parsed_log) is None
+
+        log = reader.process(as_bytes=False)
+        parsed_log = parser.process(log)
+        assert detector.process(parsed_log) is not None
