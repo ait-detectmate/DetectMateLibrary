b464465d87ce205246f217b3b9bb0442d689e8a8
Anna Erdi <erdianna607@gmail.com>
2025-10-14 16:00:34 +0200

    Try importing components with redirect

---
+++ src/detectmatelibrary/detectors/__init__.py
@@ -0,0 +1,4 @@
+from ...components.detectors import RandomDetector, RandomConfig
+from ...components.detectors import NewValueDetector, NVDConfig
+
+__all__ = ["RandomDetector", "RandomConfig", "NewValueDetector", "NVDConfig"]
