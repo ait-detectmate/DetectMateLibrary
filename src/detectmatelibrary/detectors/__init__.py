17e5b0d41b96fa3262a49eaa27ee0bb2d96a1405
Anna Erdi <erdianna607@gmail.com>
2025-10-14 17:08:40 +0200

    Make detector classes available at package level

---
+++ src/detectmatelibrary/detectors/__init__.py
@@ -0,0 +1,9 @@
+from .RandomDetector import RandomDetector, RandomConfig
+from .NewValueDetector import NewValueDetector, NVDConfig
+
+__all__ = [
+    "RandomDetector",
+    "RandomConfig",
+    "NewValueDetector",
+    "NVDConfig"
+]
