162ebd524917ede16fb1a16481aa3f5386da1017
Ernst Leierzopf <e.leierzopf@gmail.com>
2026-04-01 21:49:20 +0200

    Revert "Merge branch 'feature/new-event-detector' into development"
    This reverts commit 3b21465af251be31cf13756216ffd60dabae6ff3, reversing
    changes made to 3ad3c584aefcb6307a031ebf0ed2bb98ee95c031.

--- src/detectmatelibrary/detectors/__init__.py
+++ src/detectmatelibrary/detectors/__init__.py
@@ -1,13 +1,10 @@
 from .random_detector import RandomDetector, RandomDetectorConfig
 from .new_value_detector import NewValueDetector, NewValueDetectorConfig
-from .new_event_detector import NewEventDetector, NewEventDetectorConfig
 
 __all__ = [
     "random_detector",
     "RandomDetectorConfig",
     "NewValueDetector",
     "NewValueDetectorConfig",
-    "RandomDetector",
-    "NewEventDetector",
-    "NewEventDetectorConfig"
+    "RandomDetector"
 ]
