d1a1021ae547c4d9fdfdc72eeb6e90646bb0bd53
angre.garcia-gomez@ait.ac.at <angre.garcia-gomez@ait.ac.at>
2026-04-16 17:11:47 +0200

    start adding rules

--- src/detectmatelibrary/detectors/rule_detector.py
+++ src/detectmatelibrary/detectors/rule_detector.py
@@ -1,3 +1,9 @@
+"""Copy and paste to create new rules.
+
+def template_rule(input_: schemas.ParserSchema, *args: list[Any]) ->
+tuple[bool, str]:     raise_alert = False  # Add here rule     message =
+""  # Rule message     return raise_alert, message
+"""
 from detectmatelibrary.common.detector import CoreDetector, CoreDetectorConfig
 
 from detectmatelibrary.utils.data_buffer import BufferMode
@@ -7,8 +13,35 @@ from detectmatelibrary import schemas
 from typing import List, Any
 
 
+def template_not_found(input_: schemas.ParserSchema, *args: list[Any]) -> tuple[bool, str]:
+    raise_alert = input_["EventID"] == -1
+    message = "Template was not found by the parser"
+    return raise_alert, message
+
+
+def find_keyword(input_: schemas.ParserSchema, args: list[str]) -> tuple[bool, str]:
+    log: str = input_["log"]
+    log = log.lower()
+
+    for k in args:
+        if k in log:
+            return True, f"Found word '{k}' in the logs"
+    return False, ""
+
+
+def exceptions(input_: schemas.ParserSchema, *args: list[Any]) -> tuple[bool, str]:
+    return find_keyword(input_, ["exception", "fail", "error", "raise"])
+
+
+rules = {
+    "TemplateNotFound": template_not_found,
+    "SpecificKeyword": find_keyword,
+    "CheckForExceptions": exceptions,
+}
+
+
 class RuleDetectorConfig(CoreDetectorConfig):
-    method_type: str = "dummy_detector"
+    method_type: str = "rule_detector"
 
 
 class RuleDetector(CoreDetector):
---
+++ tests/test_detectors/test_rule_detector.py
@@ -0,0 +1,32 @@
+import detectmatelibrary.detectors.rule_detector as rd
+import detectmatelibrary.schemas as schemas
+
+
+class TestCaseRules:
+    def test_template_not_found(self) -> None:
+        parsed_log = schemas.ParserSchema({"EventID": -1})
+
+        alert, msg = rd.template_not_found(parsed_log)
+        assert alert
+        assert "Template was not found by the parser" == msg
+
+        parsed_log = schemas.ParserSchema({"EventID": 2})
+        alert, _ = rd.template_not_found(parsed_log)
+        assert not alert
+
+    def test_find_keyword(self) -> None:
+        keywords = ["hello", "ciao"]
+
+        parsed_log = schemas.ParserSchema({"log": "hello world"})
+        alert, msg = rd.find_keyword(parsed_log, *keywords)
+        assert alert
+        assert "Found word 'hello' in the logs" == msg
+
+        parsed_log = schemas.ParserSchema({"log": "ciao world"})
+        alert, msg = rd.find_keyword(parsed_log, *keywords)
+        assert alert
+        assert "Found word 'ciao' in the logs" == msg
+
+        parsed_log = schemas.ParserSchema({"log": "world"})
+        alert, _ = rd.find_keyword(parsed_log, *keywords)
+        assert not alert
