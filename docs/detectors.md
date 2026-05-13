5202bed16d5b5bc0b5bae664f9a15654ff6618cf
Ernst Leierzopf <e.leierzopf@gmail.com>
2026-05-13 08:04:33 +0200

    implement CharsetDetector.

--- docs/detectors.md
+++ docs/detectors.md
@@ -89,6 +89,7 @@ List of detectors:
 * [Combo Detector](detectors/combo.md): Detect new combination of variables in the logs.
 * [New Event](detectors/new_event.md): Detect new events in the variables in the logs.
 * [Rule Based](detectors/rule_based.md): Detect anomalies based in a set of rules.
+* [Charset](detectors/charset.md): Detect new characters in the variables in the logs.
 
 ## Configuration
 
--- src/detectmatelibrary/detectors/__init__.py
+++ src/detectmatelibrary/detectors/__init__.py
@@ -1,6 +1,7 @@
 from .random_detector import RandomDetector, RandomDetectorConfig
 from .new_value_detector import NewValueDetector, NewValueDetectorConfig
 from .new_event_detector import NewEventDetector, NewEventDetectorConfig
+from .charset_detector import CharsetDetector, CharsetDetectorConfig
 
 __all__ = [
     "random_detector",
@@ -9,5 +10,7 @@ __all__ = [
     "NewValueDetectorConfig",
     "RandomDetector",
     "NewEventDetector",
-    "NewEventDetectorConfig"
+    "NewEventDetectorConfig",
+    "CharsetDetector",
+    "CharsetDetectorConfig"
 ]
