bb2a7e2ec7a0ea0614e9e96a5f8e74719bfe5db1
Ernst Leierzopf <e.leierzopf@gmail.com>
2026-05-18 21:08:31 +0200

    Add dynamic version to pyproject.toml. Remove setup.py. Add metadata.py.

--- src/detectmatelibrary/__init__.py
+++ src/detectmatelibrary/__init__.py
@@ -3,11 +3,14 @@ from . import detectors
 from . import parsers
 from . import schemas
 from . import utils
+from .metadata import *
+from .metadata import __all__ as metadata_all
 
 __all__ = [
     "common",
     "detectors",
     "parsers",
     "schemas",
-    "utils"
+    "utils",
+    *metadata_all,
 ]
