88f913e04a421adffb36c81a219bf143436eb04b
Ernst Leierzopf <e.leierzopf@gmail.com>
2026-07-06 20:01:03 +0200

    fix create workspace.

--- src/detectmatelibrary/tools/workspace/create_workspace.py
+++ src/detectmatelibrary/tools/workspace/create_workspace.py
@@ -7,7 +7,7 @@ from .utils import create_readme, create_pyproject, normalize_package_name
 
 # resolve paths relative to this file
 BASE_DIR = Path(__file__).resolve().parent.parent  # tools/
-PROJECT_ROOT = BASE_DIR.parent.parent.parent  # root of project
+PROJECT_ROOT = BASE_DIR.parent.parent      # root of project
 TEMPLATE_DIR = BASE_DIR / "workspace" / "templates"
 
 META_FILES = ["LICENSE.md", ".gitignore", ".pre-commit-config.yaml"]
