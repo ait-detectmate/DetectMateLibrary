eed0df8493993fae0df3b561f14c7e2583112300
angre.garcia-gomez@ait.ac.at <angre.garcia-gomez@ait.ac.at>
2026-07-24 15:51:53 +0200

    adding autoconfig

--- src/detectmatelibrary/parsers/drain.py
+++ src/detectmatelibrary/parsers/drain.py
@@ -4,6 +4,8 @@ from detectmatelibrary import schemas
 from detectmateperformance.match_tree import TreeMatcher
 from detectmateperformance.drain import Drain
 
+from detectmatelibrary.utils.finetune import Combinations
+
 from typing import Any
 
 
@@ -29,6 +31,18 @@ def _init_drain(config: DrainConfig) -> Drain:
     )
 
 
+def _found_ratio(logs: list[str], tree_matcher: TreeMatcher) -> float:
+    results = tree_matcher.match_batch(logs).get_all_templates()
+    print(results)
+
+    score = 0.0
+    for template in results:
+        if "template not found" == template:
+            score += 1.
+
+    return score / len(results)
+
+
 class DrainParser(CoreParser):
     def __init__(
         self,
@@ -44,14 +58,28 @@ class DrainParser(CoreParser):
         self.drain_gen = _init_drain(config=config)
         self.tree_match: TreeMatcher | None = None
 
-        self.report: dict[str, float] = {}
-        self.config_buffer: list[schemas.LogSchema] = []
+        self.config_buffer: list[str] = []
 
     def configure(self, input_: schemas.LogSchema) -> None:  # type: ignore
-        self.config_buffer.append(input_)
+        self.config_buffer.append(input_["log"])
 
     def set_configuration(self) -> None:
-        pass
+        found_ratio: list[float] = []
+        length: list[int] = []
+        for config in (comb := Combinations(self.config))():
+            drain = _init_drain(config)  # type: ignore
+            for input_ in self.config_buffer:
+                drain.add(input_)
+            tree_matcher = drain.generate()
+
+            found_ratio.append(_found_ratio(self.config_buffer, tree_matcher))
+            length.append(len(tree_matcher))
+
+        n = max(length)
+        for le, sc in zip(length, found_ratio):
+            comb.add_value((float(le) / n) + sc)
+
+        self.config = comb.get_best()  # type: ignore
 
     def train(self, input_: schemas.LogSchema) -> None:  # type: ignore
         self.drain_gen.add(input_["log"])
--- tests/test_parsers/test_drain.py
+++ tests/test_parsers/test_drain.py
@@ -1,5 +1,9 @@
 """Most of the functionality is test it in DetectMatePerformance."""
-from detectmatelibrary.parsers.drain import DrainParser
+from detectmatelibrary.parsers.drain import DrainParser, _found_ratio
+
+from detectmateperformance.match_tree import TreeMatcher
+from detectmateperformance.types_ import LogTemplates
+
 from detectmatelibrary import schemas
 
 
@@ -103,15 +107,34 @@ class TestDrainParser:
         parsed = parser.process(schemas.LogSchema({"log": "hello there, sargent kenobi!"}))
         assert parsed["template"] == "hello there <*> kenobi"
 
+    def test_not_ration_found(self):
+        tree_matcher = TreeMatcher(LogTemplates(["hello there <*> kenobi"]))
+
+        logs = ["hello there general kenobi", "akuna matata"]
+        assert 0.5 == _found_ratio(logs, tree_matcher)
+
+        logs = ["hello there general kenobi"]
+        assert 0. == _found_ratio(logs, tree_matcher)
+
     def test_no_auto_config_but_no_initialization(self):
         config_dict = {
             "parsers": {
                 "DrainParser": {
                     "method_type": "drain_parser",
+                    "depth": 2,
+                    "max_childs": 10,
+                    "sim_thres": 0.2,
                     "auto_config": True,
                     "data_use_configure": 2,
                     "data_use_training": 2,
                 }
             }
         }
-        DrainParser(config=config_dict)
+        parser = DrainParser(config=config_dict)
+        parser.process(schemas.LogSchema({"log": "hello there, general kenobi!"}))
+        parser.process(schemas.LogSchema({"log": "hello there, captain kenobi!"}))
+        parser.process(schemas.LogSchema({"log": "hello there, captain kenobi!"}))
+
+        assert parser.config.depth == 1
+        assert parser.config.max_childs == 10
+        assert parser.config.sim_thres == 0.2
